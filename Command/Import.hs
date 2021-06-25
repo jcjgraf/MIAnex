b4a1fedb45c90b668aaafdb8a45bbc51ca912d07
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-06-25 22:56:34 +0200

    NEW: functionality for different import commands
    Functionality for new, activate, deactivate and list

--- Command/Import.hs
+++ Command/Import.hs
@@ -1,9 +1,30 @@
 module Command.Import where
 
-import Command (ImportOptions)
+import Command (ImportOptions(..))
+import qualified Config as Conf
+import qualified Git as Git
 
 runImport :: ImportOptions -> IO ()
-runImport opts = do
-    putStrLn "runImport"
+runImport (ImportNew name) = do
     return ()
 
+runImport (ImportActivate name) = do
+    exists <- Git.branchExists name
+    case exists of
+        True -> do
+            _ <- Git.runGit ["checkout", name]
+            return ()
+        False -> putStrLn "Error, branch does not exists"
+    return ()
+
+runImport (ImportDeactivate True) = do
+    _ <- Git.runGit ["checkout", Conf.mainBranch]
+    return ()
+
+runImport (ImportList True) = do
+    branches <- Git.getBranches
+    putStrLn $ unlines branches
+    return ()
+
+runImport _ = do
+    return ()
