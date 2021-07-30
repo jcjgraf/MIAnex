894fd0283901c12600cedf729d739341cbbfde61
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-07-30 21:06:54 +0200

    NEW: XDG Based config file

--- Config.hs
+++ Config.hs
@@ -2,20 +2,25 @@ module Config where
 
 import Data.Ini
 import Data.Text
+import System.Directory
 
 getOption :: String -> String -> IO String
 getOption group option = do
-    config <- readIniFile "/home/jeanclaude/.config/mia/config.ini"
-    -- TODO xdg base dir
-    -- TODO case when file missing
-    case config of
-        Right ini -> do
-            let p = lookupValue (pack group) (pack option) ini
-            return $ case p of
-                        Left s -> s
-                        Right t -> unpack t
-        Left s -> do
-            putStrLn s
+    confFile <- getXdgDirectory XdgConfig "mia/config.ini"
+    exists <- doesFileExist confFile
+    if exists then do
+        config <- readIniFile confFile
+        case config of
+            Right ini -> do
+                let p = lookupValue (pack group) (pack option) ini
+                return $ case p of
+                            Left s -> s
+                            Right t -> unpack t
+            Left s -> do
+                putStrLn s
+                return ""
+        else do
+            putStrLn $ "Config " ++ (show confFile) ++ " does not exist"
             return ""
 
 archivePath :: IO String
