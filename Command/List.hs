531b638716c692966c89e8aa66c00f1b6a858ab9
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-10-27 14:03:18 +0200

    IMPROVE: Filter list to show imports only

--- Command/List.hs
+++ Command/List.hs
@@ -1,10 +1,20 @@
 module Command.List where
 
+import Data.Text (pack, isInfixOf)
+import qualified Config as Conf
+
 import qualified Git as Git
 
 
 runList :: IO ()
 runList = do
     branches <- Git.getBranches
-    putStrLn $ unlines branches
+    mainBranch <- Conf.mainBranch
+
+    -- TODO: make filter more clean and general
+    let filterStrings = map pack ["synced/", "git-annex", mainBranch]
+    let branchesFiltered1 = filter (\e -> not $ isInfixOf (filterStrings !! 0) (pack e)) branches
+    let branchesFiltered2 = filter (\e -> not $ isInfixOf (filterStrings !! 1) (pack e)) branchesFiltered1
+    let branchesFiltered3 = filter (\e -> not $ isInfixOf (filterStrings !! 2) (pack e)) branchesFiltered2
+    putStrLn $ unlines branchesFiltered3
     return ()
